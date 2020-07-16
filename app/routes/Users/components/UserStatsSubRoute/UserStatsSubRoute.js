ccae76967504def02e14e326c84cdc5af0c77d65
Oliver Joseph Ash <oliverjash@gmail.com>
2020-07-16 13:37:19 +0100

    WIP

--- app/routes/Users/components/UserStatsSubRoute/UserStatsSubRoute.js
+++ app/routes/Users/components/UserStatsSubRoute/UserStatsSubRoute.js
@@ -5,10 +5,11 @@ import PhotoFeed from 'components/PhotoFeed';
 import StatsFilter from './components/StatsFilter';
 import StatsGraph from './components/StatsGraph';
 
-const UserStatsSubRoute = () => (
+export default () => (
   <div>
     <StatsFilter />
     <StatsGraph />
     <PhotoFeed />
   </div>
 );
+
