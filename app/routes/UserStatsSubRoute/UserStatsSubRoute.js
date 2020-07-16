1f3eafd3af71c25b4e174071409840c869f25add
Thomas Lefebvre <thomas@unsplash.com>
2020-07-16 09:30:04 -0400

    WIP

--- app/routes/UserStatsSubRoute/UserStatsSubRoute.js
+++ app/routes/UserStatsSubRoute/UserStatsSubRoute.js
@@ -1,12 +1,10 @@
 import React from 'react';
 
 import PhotoFeed from './components/PhotoFeed/PhotoFeed';
-import StatsFilter from './components/StatsFilter/StatsFilter';
 import StatsGraph from './components/StatsGraph/StatsGraph';
 
 export default () => (
   <div>
-    <StatsFilter />
     <StatsGraph />
     <PhotoFeed />
   </div>
