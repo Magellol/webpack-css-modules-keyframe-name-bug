36a9bd95ed3277f5ade049eea27d96a25b270169
Oliver Joseph Ash <oliverjash@gmail.com>
2020-07-16 19:51:43 +0100

    WIP

--- app/index.js
+++ app/index.js
@@ -1,3 +1,6 @@
+// This is only here to slow down the build
+// The issue seems to happen more frequently when the build is slower
 require('lodash');
+
 require("./components/A");
 require("./components/B");
